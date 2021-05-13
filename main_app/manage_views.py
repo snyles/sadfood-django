91d40b66ae3a476101c84d383157650f7b2311d9
snyles <sniles@gmail.com>
2021-05-13 10:44:12 -0400

    photo upload working locally

--- main_app/manage_views.py
+++ main_app/manage_views.py
@@ -5,10 +5,31 @@ from django.contrib.auth.mixins import UserPassesTestMixin
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
-from .models import Item, Category, Profile
+from .models import Item, Category, Profile, Photo
+import uuid
+import boto3
+
+S3_BASE_URL = 'https://s3.us-east-2.amazonaws.com/'
+BUCKET = 'sadfoodtoys'
 
 def is_manager(user):
   return user.groups.filter(name='Manager').exists()
+
+@user_passes_test(is_manager)
+def add_photo(request, item_id):
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+    s3 = boto3.client('s3')
+    key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+    try:
+      s3.upload_fileobj(photo_file, BUCKET, key)
+      url = f"{S3_BASE_URL}{BUCKET}/{key}"
+      photo = Photo(url=url, item_id=item_id)
+      photo.save()
+    except Exception as err:
+      print('An error occurred uploading file to S3: %s' % err)
+  return redirect('item_list', item_id=item_id)
+
 
 class ItemCreate(UserPassesTestMixin, CreateView):
   model = Item
--- main_app/models.py
+++ main_app/models.py
@@ -43,4 +43,9 @@ class Profile(models.Model):
   def __str__(self):
     return self.user.username
   
+class Photo(models.Model):
+  url = models.CharField(max_length=200)
+  item = models.ForeignKey(Item, on_delete=models.CASCADE)
 
+  def __str__(self):
+    return f"Photo for item_id: {self.item_id} @{self.url}"
