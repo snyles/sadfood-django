0f231e7534913dee00b539ee8956b0d1761e33e7
snyles <sniles@gmail.com>
2021-05-13 13:14:09 -0400

    update item detail

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,9 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-  # path('', views.home, name='home'),
   path('', views.shop, name='shop'),
-  # path('about/', views.about, name='about'),
+  # path('favorites/', views.favorites, name='favorites'),
   path('category/<str:cat>/', views.category, name='category'),
   path('item/<int:pk>', views.ItemDetail.as_view(), name='item_detail'),
   path('manage/', include('main_app.manage_urls')),
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import user_passes_test, login_required
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -19,11 +19,23 @@ def shop(request):
 class ItemDetail(DetailView):
   model = Item
 
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['catlist'] = Category.objects.all()
+    return context
+
 def category(request, cat):
   categories = Category.objects.all()
   cat_items = Item.objects.filter(categories__name__iexact=cat)
   return render(request, 'shop.html', {
     'category': cat, 'items': cat_items, 'catlist': categories})
+
+# @login_required
+# def favorites(request):
+#   items = Profile.objects.get(user=request.user).items.all()
+#   categories = Category.objects.all()
+#   return render(request, 'shop.html', {
+#     'category': 'Favorites', 'items': items, 'catlist': categories})
 
 def signup(request):
   error_message = ''
@@ -34,7 +46,7 @@ def signup(request):
       profile = Profile(user=user)
       profile.save()
       login(request, user)
-      return redirect('index')
+      return redirect('shop')
     else:
       error_message = 'Invalid sign up - try again'
   form = UserCreationForm()
