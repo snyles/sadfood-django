a91201369dd4e10fc17e5d32ec7521608702beca
snyles <sniles@gmail.com>
2021-05-13 10:55:45 -0400

    reconfig deployment

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Profile, Category, Item
+from .models import Profile, Category, Item, Photo
 
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Category)
 admin.site.register(Item)
+admin.site.register(Photo)
